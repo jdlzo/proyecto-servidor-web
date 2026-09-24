0b4de9bd928295272efea0b8b122ee45f2f1596d
list <jordan3271@hotmail.com>
2026-09-24 12:06:48 -0500

    implementacion de conexion de postgre inicial

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
+
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'servernest',
+      entities: [],
+      synchronize: false,
+    }),
     // Distributed tracing, auto-correlated logs, request/job metrics, error
     // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
     ObserveModule.forRoot({
